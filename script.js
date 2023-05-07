ddd7ae788e3d4cdb6e8e73ce0c8fb0597d5c5a87
opalBee <76087277+opalBee@users.noreply.github.com>
2023-05-07 17:32:17 +1000

    add function to play a single round of game

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
-let weapon = ['rock', 'paper', 'scissors'];
 
+let weapon = ['rock', 'paper', 'scissors'];
+//select random array element from weapon array
 function getComputerChoice() {
-    computerChoice = weapon [ (Math.floor(Math.random() * weapon.length)) ];
+   computerChoice = weapon [ (Math.floor(Math.random() * weapon.length)) ];
     console.log(computerChoice);
 }
 
-console.log(getComputerChoice());
+
+//play a single round of Rock Paper, Scissors. Make playerSelection parameter case insensitive.
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+
+
+function playRound(playerSelection, computerSelection) {
+    
+    
+}
+
+//console.log(playRound(playerSelection, computerSelection));
+//console.log(computerSelection);
+
