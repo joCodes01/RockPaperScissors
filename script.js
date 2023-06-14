b8b73b7eaad5b55ed30136e1f39d27f35af618dd
joCodes01 <76087277+opalBee@users.noreply.github.com>
2023-06-14 11:38:49 +1000

    Add buttons to index.html.   Edit script.js to log player selection from button click

--- script.js
+++ script.js
@@ -10,65 +10,68 @@ function getComputerChoice() {
 }
 
 
-//USER CHOICE- assign user choice from prompt input
-function getPlayerChoice() {
-    playerChoice = prompt('Choose your weapon', 'rock, paper or scissors?');
-    return playerChoice.toLowerCase();;
-}
-
-
 //make global player score variables
 let playerScore = 0;
 let computerScore = 0;
 
 
-//function to play one round
+//PLAY ONE ROUND
 function playRound() {
-    //getComputerChoice();
-    //getPlayerChoice();
-
-    let playerSelection = getPlayerChoice();
-    let computerSelection = getComputerChoice();
-
-    console.log('player ', playerSelection);
-    console.log('computer ', computerSelection);
-
-    //message to return to player
-    let youWin = `You win, ${playerSelection} beats ${computerSelection}`;
-    let youLose = `You lose, ${computerSelection} beats ${playerSelection}`;
-    let youDraw = `It's a draw!`;
-    //put message options into an array
-    let message = [ youWin, youLose, youDraw];
-
-
-      if (playerSelection == computerSelection) {
-        return youDraw;
-    } else if (playerSelection == 'rock' && computerSelection == 'paper') {
-        computerScore++;
-        return message[1]; //you lose
-    } else if (playerSelection == 'rock' && computerSelection == 'scissors') {
-        playerScore ++;
-        return message[0]; //you win
-    } else if (playerSelection == 'paper' && computerSelection == 'rock') {
-        playerScore ++;
-        return message[0]; //you win
-    } else if (playerSelection == 'paper' && computerSelection == 'scissors') {
-        computerScore ++;
-        return message[1]; //you lose
-    } else if (playerSelection == 'scissors' && computerSelection == 'rock') {
-        computerScore ++;
-        return message[1]; //you lose
-    } else if (playerSelection == 'scissors' && computerSelection == 'paper') {
-        playerScore ++;
-        return message[0]; //you win
-    } else {
-        return('oops! Type rock, paper or scissors!')
-    }
     
-}
+    //USER CHOICE from button click
+    const buttons = document.querySelectorAll('button');
 
+    let playerSelection;
+
+    buttons.forEach((button) => {
+        button.addEventListener('click', () => {
+            playerSelection = button.className;
+            const computerSelection = getComputerChoice();
+    
+            console.log("selection: " + playerSelection);
+            console.log("comp selection: " + getComputerChoice());
+
+                //message to return to player
+                let youWin = `You win, ${playerSelection} beats ${computerSelection}`;
+                let youLose = `You lose, ${computerSelection} beats ${playerSelection}`;
+                let youDraw = `It's a draw!`;
+                //put message options into an array
+                let message = [ youWin, youLose, youDraw];
+
+
+                if (playerSelection == computerSelection) {
+                    console.log("youDraw") ;
+                } else if (playerSelection == 'rock' && computerSelection == 'paper') {
+                    computerScore++;
+                    console.log(message[1]); //you lose
+                } else if (playerSelection == 'rock' && computerSelection == 'scissors') {
+                    playerScore ++;
+                    console.log(message[0]); //you win
+                } else if (playerSelection == 'paper' && computerSelection == 'rock') {
+                    playerScore ++;
+                    console.log(message[0]); //you win
+                } else if (playerSelection == 'paper' && computerSelection == 'scissors') {
+                    computerScore ++;
+                    console.log(message[1]); //you lose
+                } else if (playerSelection == 'scissors' && computerSelection == 'rock') {
+                    computerScore ++;
+                    console.log(message[1]); //you lose
+                } else if (playerSelection == 'scissors' && computerSelection == 'paper') {
+                    playerScore ++;
+                    console.log(message[0]); //you win
+                } else {
+                    console.log('oops! Type rock, paper or scissors!');
+                }
+                
+            } )
+        })
+    }
+
+        
+playRound();
 
 //function to play five rounds and report player as winner or loser at the end
+/*
 function game() {
 
      console.log(playRound());
@@ -78,9 +81,9 @@ function game() {
      console.log(playRound());
 
      if (playerScore > computerScore) {
-        return 'You win this round';
+        return 'Congratulations! You win this round';
     } else if (playerScore < computerScore) {
-        return 'You lose this round'; 
+        return 'Bad luck! You lose this round'; 
     } else if (playerScore == computerScore) {
         'It\'s a draw!'; 
      } else {
@@ -88,8 +91,25 @@ function game() {
     }
 }
 
-
-console.log(game());
+//console.log(game());
 console.log('player score result ', playerScore);
 console.log('computer score result ',computerScore);
+*/
+
+
+
+
+/*
+// SHOW RESULTS TO USER
+
+let playerWeapon = document.querySelector('#container2');
+container2.style.backgroundColor = 'lightBlue';
+let playerHeader = document.createElement('h2');
+playerHeader.classList.add('playerHeader');
+playerHeader.textContent = " The players selection is: ";
+container2.appendChild(playerHeader);
+*/
+
+
+
 
